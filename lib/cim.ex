aae5b94a6ada3a2a4959a22cff0df78822f82e25
thang.pham <thang.pham@cogo.co>
2022-09-13 16:49:35 +1200

    remove boilerplate cim.ex

--- lib/cim.ex
+++ lib/cim.ex
@@ -1,18 +1,3 @@
 defmodule Cim do
-  @moduledoc """
-  Documentation for `Cim`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cim.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
